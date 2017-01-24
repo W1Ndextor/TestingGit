7e5fa3470f830098336cb842a768b8e3080d6e8e
W1Ndextor <straylinej@chc.edu>
2017-01-24 08:51:58 -0500

    Version 1

---
+++ src/testinggit/Movie.java
@@ -0,0 +1,46 @@
+
+package testinggit;
+
+/**
+ *
+ * @author Josh
+ */
+public class Movie {
+    
+    private String title;
+    private String author;
+    private int numberPages;
+
+    public Movie(String title, String author, int numberPages) {
+        this.title = title;
+        this.author = author;
+        this.numberPages = numberPages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumberPages() {
+        return numberPages;
+    }
+
+    public void setNumberPages(int numberPages) {
+        this.numberPages = numberPages;
+    }
+    
+    
+    
+}
---
+++ src/testinggit/TestingGit.java
@@ -0,0 +1,17 @@
+
+package testinggit;
+
+/**
+ *
+ * @author Josh
+ */
+public class TestingGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
