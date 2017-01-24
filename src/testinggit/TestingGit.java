b8e816a3c619209557937338cd6185907c69d3f4
W1Ndextor <straylinej@chc.edu>
2017-01-24 09:15:41 -0500

    Version 3

--- src/testinggit/TestingGit.java
+++ src/testinggit/TestingGit.java
@@ -13,6 +13,7 @@ public class TestingGit {
     public static void main(String[] args) {
         
         Movie Carrie = new Movie("Carrie", "Stephen King", 180);
+        Movie theShining = new Movie ("The Shining", "Stephen KIng", 330);
     }
     
 }
